d6e3948fbd01edd7ff6c1d2075b03d09b55c1907
Ripon Ahmed <ripondxn@gmail.com>
2025-08-19 13:57:03 +0000

    replicate the "Properties list page " edit button function to "Unit page

--- src/app/property/units/columns.tsx
+++ src/app/property/units/columns.tsx
@@ -12,7 +12,6 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Checkbox } from '@/components/ui/checkbox';
@@ -102,11 +101,23 @@ export const columns: ColumnDef<Unit>[] = [
 
       return (
         <div className="text-right">
-            <Button asChild variant="ghost" size="icon">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem asChild>
                 <Link href={`/property/unit?unitCode=${unit.unitCode}`}>
-                    <Pencil className="h-4 w-4" />
+                  <Pencil className="mr-2 h-4 w-4" />
+                  Edit
                 </Link>
-            </Button>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       );
     },
