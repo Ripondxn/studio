106049bf7d9b4904969b53245a4d5e5ab0bcbb9f
Ripon Ahmed <ripondxn@gmail.com>
2025-08-20 05:49:16 +0000

    remove " Dashboard" item from Top menu

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -76,7 +76,6 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
   }
   
   const navLinks = [
-    { href: '/', label: 'Dashboard', icon: <LayoutDashboard /> },
     { 
         label: 'Leases', 
         icon: <FileSignature />,
@@ -111,7 +110,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
         <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-40">
             <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
                 <Link
-                href="#"
+                href="/"
                 className="flex items-center gap-2 text-lg font-semibold md:text-base"
                 >
                 <Building2 className="h-6 w-6 text-primary" />
