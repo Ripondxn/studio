637010ffe7ea03ed801800c88816dfa586779db4
Ripon Ahmed <ripondxn@gmail.com>
2025-08-21 18:11:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/tenancy/customer/invoice/columns.tsx
+++ src/app/tenancy/customer/invoice/columns.tsx
@@ -16,6 +16,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { format } from 'date-fns';
 import { type Invoice } from './schema';
