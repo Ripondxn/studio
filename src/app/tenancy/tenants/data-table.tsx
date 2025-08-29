8dc745c65f5a955e43c191d190f9e919a6c7cfc1
Ripon Ahmed <ripondxn@gmail.com>
2025-08-29 07:31:24 +0000

    set default sorting assending order by tenat code under tenant page whil

--- src/app/tenancy/tenants/data-table.tsx
+++ src/app/tenancy/tenants/data-table.tsx
@@ -41,7 +41,9 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
-  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [sorting, setSorting] = React.useState<SortingState>([
+    { id: 'code', desc: false },
+  ]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
   );
