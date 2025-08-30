282931cb7005a38cb72a3179d773307066ec6e7b
Ripon Ahmed <ripondxn@gmail.com>
2025-08-30 09:08:56 +0000

    still invalid data format

--- src/app/finance/payment/actions.ts
+++ src/app/finance/payment/actions.ts
@@ -13,7 +13,7 @@ import { applyPaymentToInvoices } from '@/app/tenancy/customer/invoice/actions';
 import { type Contract as TenancyContract } from '@/app/tenancy/contract/schema';
 import { type LeaseContract } from '@/app/lease/contract/schema';
 import { type Invoice } from '@/app/tenancy/customer/invoice/schema';
-import { type Bill } from '@/app/vendors/bill/schema';
+import { type Bill } from '@/app/vendors/bill/schema-def';
 import { type Cheque } from '../cheque-deposit/schema';
 import { getWorkflowSettings } from '@/app/admin/workflow-settings/actions';
 import { applyFinancialImpact } from '@/app/workflow/actions';
